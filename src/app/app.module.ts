2c0c189b0fc077996e68bb39f39a0f1ae96b2934
mikedobor <micki1.com@gmail.com>
2018-06-13 10:49:24 +0100

    Created custom material theme and installed angular flex layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
+import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatInputModule, MatCheckboxModule, MatFormFieldModule, MatDatepickerInput, MatRadioModule } from '@angular/material';
+import { MatInputModule, MatCheckboxModule, MatFormFieldModule, MatRadioModule, MatButtonModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './pages/register/register.component';
@@ -15,12 +17,13 @@ import { LoginComponent } from './pages/login/login.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     BrowserAnimationsModule,
     MatInputModule,
     MatCheckboxModule,
     MatFormFieldModule,
-    MatDatepickerInput,
-    MatRadioModule
+    MatRadioModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
