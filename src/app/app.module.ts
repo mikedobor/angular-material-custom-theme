e60ff82418561eaa44a3477ad910f05856ee5247
mikedobor <micki1.com@gmail.com>
2018-06-13 09:09:52 +0100

    Generated register and login pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule, MatCheckboxModule, MatFormFieldModule, MatDatepickerInput, MatRadioModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
