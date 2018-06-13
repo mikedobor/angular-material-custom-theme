0cbaddb295a0d15706cf69a5c34c283f6a91f492
mikedobor <micki1.com@gmail.com>
2018-06-13 08:52:41 +0100

    Added imports in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule, MatCheckboxModule, MatFormFieldModule, MatDatepickerInput, MatRadioModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,13 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatDatepickerInput,
+    MatRadioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
